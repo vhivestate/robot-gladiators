697d7528ce96f9d8c67dedb51760ed95c379a4b8
vhivestate <garcia.valeria001@gmail.com>
2022-03-28 18:45:36 -0600

    first commit

---
+++ assets/js/game.js
@@ -0,0 +1,8 @@
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+fight();
+
+var playerName window.prompt("What is your robot's name?");
